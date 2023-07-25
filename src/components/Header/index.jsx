4fbaef13b9bfd6169ddfc6689854309b3d213bce
ianRickmers <ian.rickmers@usach.cl>
2023-07-25 00:27:36 -0400

    add sass

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
         <header class="header">
             <nav class="header__navbar">
                 <a class="header__logo" href="./index.html">
-                    <img class="header__img" src="/cat_logo.webp"></img>
+                    <img class="header__img" src="/proyecto-final-frontend/cat_logo.webp"></img>
                     <i class="header__link header__link--red" href="./index.html">Cats Inc.</i>
                 </a>
                 <a class="header__link" href="./index.html">Home</a>
